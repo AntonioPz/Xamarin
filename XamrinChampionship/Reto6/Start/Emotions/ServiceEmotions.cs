d9415060352ba4548a8921542e885939b5913d86
Antonio Perez <Antonio_dddd@hotmail.es>
2017-11-11 14:09:07 -0600

    Upload: Local projects

---
+++ XamrinChampionship/Reto6/Start/Emotions/ServiceEmotions.cs
@@ -0,0 +1,23 @@
+﻿using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using Microsoft.ProjectOxford.Emotion;
+namespace Emotions
+{
+    public class ServiceEmotions
+    {
+        static string key = "d25b8f132af142a5b85afd2dbc1916b8";
+        public static async Task<Dictionary<string, float>> GetEmotions(Stream
+       stream)
+        {
+            EmotionServiceClient cliente = new EmotionServiceClient(key);
+            var emotions = await cliente.RecognizeAsync(stream);
+            if (emotions == null || emotions.Count() == 0)
+                return null;
+            return emotions[0].Scores.ToRankedList().ToDictionary(x => x.Key, x =>
+           x.Value);
+        }
+    }
+}
+
---
+++ XamrinChampionship/Reto7/Start/StorageDroid/StorageDroid.Android/MainActivity.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace StorageDroid.Droid
+{
+	[Activity (Label = "StorageDroid.Android", MainLauncher = true, Icon = "@drawable/icon")]
+	public class MainActivity : Activity
+	{
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			// Set our view from the "main" layout resource
+			SetContentView (Resource.Layout.Main);
+
+			// Get our button from the layout resource,
+			// and attach an event to it
+			Button button = FindViewById<Button> (Resource.Id.myButton);
+
+            button.Click += Button_Click;
+		}
+
+        private async void Button_Click(object sender, EventArgs e)
+        {
+            StorageDroid.StorageService storageSvc = new StorageService();
+            await storageSvc.performBlobOperation("antonio_dddd@hotmail.es");
+
+            try
+            {
+                ServiceHelper serviceHelper = new ServiceHelper();
+                string AndroidId = Android.Provider.Settings.Secure.GetString(ContentResolver, Android.Provider.Settings.Secure.AndroidId);
+
+                    Toast.MakeText(this, "Enviando tu registro", ToastLength.Short).Show();
+                    await serviceHelper.InsertarEntidad("antonio_dddd@hotmail.es", "Reto7 + antonio_dddd@hotmail.es", AndroidId);
+                    Toast.MakeText(this, "Gracias por registrarte", ToastLength.Long).Show();
+            }
+            catch (Exception exc)
+            {
+                Toast.MakeText(this, exc.Message, ToastLength.Long).Show();
+            }
+
+        }
+    }
+}
+
+
