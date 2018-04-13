698f940b091a6e9abd3eb342a661ae3be79a0c56
Antonio Perez <Antonio_dddd@hotmail.es>
2018-04-13 04:16:16 -0500

    Update: MVVM

---
+++ MVVM/MVVMCP/MVVMCP/MVVMCP/ViewModels/MainViewModel.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using System.Windows.Input;
+
+namespace MVVMCP.ViewModels  
+{
+    public class MainViewModel : ViewModelBase
+    {
+        //private string _myMessage;
+        //public MainViewModel()
+        //{
+        //    MyMessage = "Hello MVVM";
+        //}
+        //public string MyMessage
+        //{
+        //    get { return _myMessage; }
+        //    set
+        //    {
+        //        _myMessage = value;
+        //        RaisePropertyChanged("MyMessage");
+        //    }
+        //}
+
+
+
+
+
+        private int _counter;
+        private DelegateCommand _myCommand;
+
+        void counterCommandExecute()
+        {
+            _counter++;
+            RaisePropertyChanged("MyMessage");
+        }
+
+        public MainViewModel()
+        {
+            _counter = 0;
+        }
+
+        public string MyMessage
+        {
+            get { return string.Format("{0} times", _counter); }
+        }
+
+        public ICommand MyCommand
+        {
+            get { return _myCommand = _myCommand ?? new DelegateCommand(counterCommandExecute); }
+        }
+
+    }
+}
---
+++ MVVM/MVVMCP/MVVMCP/MVVMCP/ViewModels/ViewModelBase.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace MVVMCP.ViewModels
+{
+    public class ViewModelBase: INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void RaisePropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            var handle = PropertyChanged;
+            if (handle != null)
+                handle(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
